35c3339190d75ce8c1d4023bed90898eed23878d
unknown <sameerkisgr8@gmail.com>
2022-07-08 21:23:09 -0400

    Stage 3: Game Logic

--- board.py
+++ board.py
@@ -4,6 +4,16 @@ board = [
     ['-','-','-']
 ]
 player = 1
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 def printBoard():
     global board
@@ -24,11 +34,6 @@ def placeMark(row, column):
     else:
         board[row-1][column-1] = "O"
 
-def game():
-    printBoard()
-    turn()
-    game()
-
 def turn():
     global player
     invalid = True
@@ -38,9 +43,41 @@ def turn():
         column = int(input("Column: "))
         invalid = checkMark(row, column)
     placeMark(row, column)
-    player = 1 if player == 2 else 2
-    
-game()
-    
 
+def checkWin():
+    global player, board
+    won = False
+    newBoard = []
+    for i in range(0,len(board)):
+        for j in range(0,len(board)):
+            newBoard += [board[i][j]]
+    for i in range(0, 7):
+        winningCondition = winningConditions[i]
+        first = newBoard[winningCondition[0]]
+        second = newBoard[winningCondition[1]]
+        third = newBoard[winningCondition[2]]
+        if (first == '-' or second == '-' or third == '-'):
+            continue
+        if (first == second and second == third):
+            won = True
+            break
+    if (won == True):
+        if (player == 1):
+            print("Player 1 won")
+            print("Player 2 lost")
+        else:
+            print("Player 2 won")
+            print("Player 1 lost")
+    elif(not("-" in "".join(newBoard))):
+        print("Draw")
+    else:
+        player = 1 if player == 2 else 2
+        game()
+
+def game():
+    printBoard()
+    turn()
+    checkWin()
+
+game()
 
