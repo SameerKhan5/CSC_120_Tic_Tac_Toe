19bdc4a75564a5275fa7ad17b1a2a119fdb83018
unknown <sameerkisgr8@gmail.com>
2022-07-08 20:09:57 -0400

    Stage 2: Taking turns

--- board.py
+++ board.py
@@ -3,9 +3,44 @@ board = [
     ['-','-','-'],
     ['-','-','-']
 ]
+player = 1
 
-def printBoard(board):
+def printBoard():
+    global board
     for row in board:
         print(row)
 
-printBoard(board)
+def checkMark(row, column):
+    global board
+    if (board[row-1][column-1] != '-'):
+        return True
+    else:
+        return False
+
+def placeMark(row, column):
+    global board, player
+    if (player == 1):
+        board[row-1][column-1] = "X"
+    else:
+        board[row-1][column-1] = "O"
+
+def game():
+    printBoard()
+    turn()
+    game()
+
+def turn():
+    global player
+    invalid = True
+    while invalid:
+        print("Player " + str(player) + "'s turn")
+        row = int(input("Row: "))
+        column = int(input("Column: "))
+        invalid = checkMark(row, column)
+    placeMark(row, column)
+    player = 1 if player == 2 else 2
+    
+game()
+    
+
+
